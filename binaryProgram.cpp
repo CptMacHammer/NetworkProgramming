c6b217483e500e7bc2083169304d2f2b671b1853
CptMacHammer <pperezcmc@gmail.com>
2016-08-29 14:11:18 -0500

    Update binaryProgram.cpp
    Added the function prototypes

--- binaryProgram.cpp
+++ binaryProgram.cpp
@@ -2,3 +2,45 @@
 //CS 3013 Network/Distributed Computing 
 //Dr. Zhao Chao
 //Sept 7, 2016
+
+//This program uses an array of boolean values to represent binary numbers. each array represents a byte 
+#include <iostream> 
+
+int main(int argc, char** args){
+  int[8] byte; 
+  
+  return 0; 
+}
+
+//generate all combinations of binary numbers from 0 to 255 and print them to the screen. 
+void generateBinary(){
+  
+}
+
+//convert the binary value of a boolean array to hex
+//input: int[8] array
+//output: char hex_value
+void  convert_bin_hex(int[] byte){
+  
+}
+
+//print a binary array to the scrren. 
+//input: int[8] array
+//output: N/A
+void  print_bin(int[] byte){
+  
+}
+
+//print the hex value of a binary array to the scrren. 
+//input: int[8] array
+//output: N/A
+void print_hex(int[] byte{
+  
+}
+
+//print the decimal value of a binary array to the scrren. 
+//input: int[8] array
+//output: N/A
+void print_dec(){
+  
+}
